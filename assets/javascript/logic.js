269b8b1d5b45abd63c7d02146d8c5aee37cf505d
Jorge Wong <JW@Jorges-MBP.attlocal.net>
2016-12-02 13:42:30 -0600

    added s to https in ajax queryURL

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -82,7 +82,7 @@ $(document).ready(function() {
             // console.log("searchQuery: " + searchQuery);
         }
 
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         $.ajax({ url: queryURL, method: "get" }).done(function(response) {
 
